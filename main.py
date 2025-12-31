b8caf3f16c02d211f096b6ba3a5c0c8063cbe9bf
212411300 <racheliseitz1@gmail.com>
2025-12-31 17:02:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import pandas as pd
 df = pd.read_csv('parkinsons.csv')
 df.head()
 
-X = df[['MDVP:Fo(Hz)', 'PPE']]
+X = df['MDVP:Fo(Hz)', 'PPE']
 y = df['status']
 print(X.head())
 print(y.head())
 
 from sklearn.preprocessing import MinMaxScaler
-
 scaler = MinMaxScaler()
 
 X_scaled = scaler.fit_transform(X)
@@ -17,7 +16,6 @@ X_scaled_df = pd.DataFrame(X_scaled, columns=X.columns)
 print(X_scaled_df.head())
 
 from sklearn.model_selection import train_test_split
-
 X_train, X_val, y_train, y_val = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
 print(f"Training set size: {X_train.shape[0]} samples")
@@ -33,8 +31,6 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_val)
 
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-
 accuracy = accuracy_score(y_val, y_pred)
-
 print(f"Current Accuracy: {accuracy:.4f}")
 
